cb02e24119bab48de1b0fe5da277637864456120
Matthew Anders <manderson0182@gmail.com>
2025-08-02 15:22:25 -0400

    Forgot to add remember to the method call

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -110,11 +110,15 @@ public static class Recursion
 
         if (remember == null)
             remember = new Dictionary<int, decimal>();
+
         if (remember.TryGetValue(s, out decimal cached))
             return cached;
 
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember)
+                    + CountWaysToClimb(s - 2, remember)
+                    + CountWaysToClimb(s - 3, remember);
         remember[s] = ways;
+
         return ways;
     }
 
