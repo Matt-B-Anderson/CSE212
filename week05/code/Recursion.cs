781cc6d025e9e69b4fc70a5856b730cb4c3cefca
Matthew Anders <manderson0182@gmail.com>
2025-08-02 15:18:31 -0400

    PermutationsChoose

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -40,7 +40,18 @@ public static class Recursion
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
-        // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            char c = letters[i];
+            string remaining = letters.Substring(0, i) + letters.Substring(i + 1);
+            PermutationsChoose(results, remaining, size, word + c);
+        }
     }
 
     /// <summary>
